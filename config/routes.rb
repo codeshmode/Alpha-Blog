13a9be4e4fafff9b6659654a64eb2643eac3ba3b
Eddie <digitalsolutionexpert@gmail.com>
2018-02-25 14:38:49 -0500

    Signup users form ui

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,9 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   
   resources :articles
+  
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
   # root 'welcome#index'
 
   # Example of regular route:
